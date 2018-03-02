0e99ad39e56cb355edad7cc6b82e4bf095e4021e
ellstang <lilyc633@gmail.com>
2018-03-02 16:17:58 +0800

    1. refined FirestVC, SecondVC 2. fixed the update name problem

--- Line-Settings/Ells/Ells/Ells/View Controller/PersonalInfoViewCOntrollerViewController.swift
+++ Line-Settings/Ells/Ells/Ells/View Controller/PersonalInfoViewCOntrollerViewController.swift
@@ -10,15 +10,16 @@ import UIKit
 
 class PersonalInfoViewController: UIViewController {
 
-  //let userdefault = UserDefaults.standard
+  let userdefault = UserDefaults.standard
   
   @IBOutlet weak var ProfilePic: UIImageView!
   @IBOutlet weak var nameToChange: UIButton!
   
   @IBOutlet weak var displayName: UILabel!
   override func viewDidLoad() {
-        super.viewDidLoad()
-    
+    super.viewDidLoad()
+    //self.ProfilePic.layer.cornerRadius = self.ProfilePic.frame.size.width / 2
+    self.ProfilePic.clipsToBounds = true
       ProfilePic.layer.cornerRadius = 60
       nameToChange.layer.cornerRadius = 10
     }
@@ -28,19 +29,33 @@ class PersonalInfoViewController: UIViewController {
       
   }
   
+  override func viewWillAppear(_ animated: Bool) {
+    func updateName(noNeed: String) {
+      userdefault.synchronize()
+      print(userdefault.data(forKey: "UpdatedName"))
+      displayName.text = userdefault.value(forKey: "UpdatedName") as? String
+      
+    }
+  }
     
   @IBAction func changeNameBtn(_ sender: UIButton) {
     
-    let changeNameVC: UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ThridVC") as! ChangeNameViewController
+    let changeNameVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ThridVC") as! ChangeNameViewController
+    changeNameVC.delegate = self
     self.present(changeNameVC, animated: true, completion: nil)
   }
-
 }
 
 extension PersonalInfoViewController: UserDefaultDelegate {
-  func updateName(userDefault: UserDefaults) {
-    userDefault.synchronize()
-    displayName.text = String(describing: userDefault.object(forKey: "UpdatedName"))
-  }
+  
+ 
+    func updateName(noNeed: String) {
+//      userdefault.synchronize()
+      let result = userdefault.value(forKey: "UpdatedName") as? String
+      print(result)
+      displayName.text = result!
+      //  String(describing: userdefault.object(forKey: "UpdatedName")!)
+      
+    }
   
 }
---
+++ Line-Settings/Ells/Ells/Ells/View Controller/RootCell.swift
@@ -0,0 +1,24 @@
+//
+//  RootCell.swift
+//  Ells
+//
+//  Created by 唐嘉伶 on 02/03/2018.
+//  Copyright © 2018 唐嘉伶. All rights reserved.
+//
+
+import UIKit
+
+class RootCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
