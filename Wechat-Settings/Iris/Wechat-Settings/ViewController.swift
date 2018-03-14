dd54199457690854988327cbe08621010e0bdc27
Iris <yuying0911@hotmail.com>
2018-03-14 21:56:26 +0800

    Add Wechat-Settings/Iris
    1st update

---
+++ Wechat-Settings/Iris/Wechat-Settings/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wechat-Settings
+//
+//  Created by Peichun Tsai on 2018/3/14.
+//  Copyright © 2018年 Peichun Tsai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
