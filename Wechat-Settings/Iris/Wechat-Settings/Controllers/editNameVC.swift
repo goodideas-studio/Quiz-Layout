9d1a93bdc769f87d89e2ecdedb62143f50a4c08c
Iris <yuying0911@hotmail.com>
2018-03-16 16:32:55 +0800

    Update Wechat-Settings/Iris
    fix bug - re-type same content saveBtn still can work

--- Wechat-Settings/Iris/Wechat-Settings/Controllers/editNameVC.swift
+++ Wechat-Settings/Iris/Wechat-Settings/Controllers/editNameVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class editNameVC: UIViewController, UITextFieldDelegate {
+class editNameVC: UIViewController {
 
     @IBOutlet weak var editTextField: UITextField!
     
@@ -16,21 +16,23 @@ class editNameVC: UIViewController, UITextFieldDelegate {
     
     @IBAction func saveBtn(_ sender: Any) {
         
+        
         if editTextField.text == "" {
-          
+
             let alert = UIAlertController(title: "", message: "必須輸入名字，請重新填寫。", preferredStyle: .alert)
             let confirmBtn = UIAlertAction(title: "我知道了", style: .default, handler: { (alert) in
                 return
             })
             alert.addAction(confirmBtn)
-            present(alert,   animated: true, completion: nil)
+            present(alert, animated: true, completion: nil)
         }
-       
+        else if editTextField.text  == userDefaults.string(forKey: "Name"){
+        }
         else {
             userDefaults.set(editTextField.text!, forKey: "Name")
             dismiss(animated: true, completion: nil)
-            
         }
+        
     }
     
     
@@ -39,14 +41,14 @@ class editNameVC: UIViewController, UITextFieldDelegate {
         let defaults = userDefaults.string(forKey: "Name")
         if defaults != editTextField.text {
             navigationItem.rightBarButtonItem?.tintColor = #colorLiteral(red: 0.4392156863, green: 0.8705882353, blue: 0.3843137255, alpha: 1)
+
         }
         else {
-            navigationItem.rightBarButtonItem?.tintColor = #colorLiteral(red: 0.2745098039, green: 0.4862745098, blue: 0.1568627451, alpha: 1)
+           navigationItem.rightBarButtonItem?.tintColor = #colorLiteral(red: 0.2745098039, green: 0.4862745098, blue: 0.1568627451, alpha: 1)
         }
     }
-   
-   
-  
+
+    @IBOutlet weak var rightBtn: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -58,7 +60,6 @@ class editNameVC: UIViewController, UITextFieldDelegate {
 
         let nameTextField = userDefaults.string(forKey: "Name")
         editTextField.text = nameTextField
-        
     
     }
 
